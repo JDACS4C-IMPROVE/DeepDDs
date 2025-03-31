91d885be0609bc2cc4eafe04f315482f643f1cac
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-03-31 16:04:26 -0400

    benchmark data - preprocess

--- deepdds_preprocess_improve.py
+++ deepdds_preprocess_improve.py
@@ -5,7 +5,8 @@ import sys
 from pathlib import Path
 from typing import Dict
 # [Req] Core improvelib imports
-from improvelib.applications.drug_response_prediction.config import DRPPreprocessConfig
+from improvelib.applications.synergy.config import SynergyPreprocessConfig
+import improvelib.applications.synergy.synergy_utils as syn
 from improvelib.utils import str2bool
 import improvelib.utils as frm
 
@@ -94,19 +95,32 @@ def run(params: Dict):
     # ------------------------------------------------------
     # Load X data (feature representations)
     # ------------------------------------------------------
-    file2 = 'data/independent_set/independent_cell_features_954.csv'
-    #file2 = cellfile
-    cell_features = []
-    with open(file2) as csvfile:
-        csv_reader = csv.reader(csvfile)  # 使用csv.reader读取csvfile中的文件
-        for row in csv_reader:
-            cell_features.append(row)
-    cell_features = np.array(cell_features)
-    print('cell_features', cell_features)
+    y_data = syn.get_all_response_data(train_split_file = params['train_split_file'], 
+                                   val_split_file = params['val_split_file'], 
+                                   test_split_file = params['test_split_file'], 
+                                   benchmark_dir = params['input_dir'])
+    cell_feature = syn.get_cell_transcriptomics(file = params['cell_transcriptomic_file'], 
+                                                  benchmark_dir = params['input_dir'], 
+                                                  cell_column_name = params['cell_column_name'], 
+                                                  norm = params['cell_transcriptomic_transform'])
+    drug_feature = syn.get_drug_mordred(file = params['drug_smiles_file'], 
+                     benchmark_dir = params['input_dir'], 
+                     drug_column_name = params['drug_column_name'])
+    
+    #file2 = 'data/independent_set/independent_cell_features_954.csv'
+    #cell_features = []
+    #with open(file2) as csvfile:
+    #    csv_reader = csv.reader(csvfile)  # 使用csv.reader读取csvfile中的文件
+    #    for row in csv_reader:
+    #        cell_features.append(row)
+    #cell_features = np.array(cell_features)
+    #print('cell_features', cell_features)
+    cell_features = np.array(cell_feature)
 
-    compound_iso_smiles = []
-    df = pd.read_csv('data/smiles.csv')
-    compound_iso_smiles += list(df['smile'])
+    #compound_iso_smiles = []
+    #df = pd.read_csv('data/smiles.csv')
+    compound_iso_smiles = list(df.iloc[:, 0])
+    #compound_iso_smiles += list(df['smile'])
     compound_iso_smiles = set(compound_iso_smiles)
     smile_graph = {}
     print('compound_iso_smiles', compound_iso_smiles)
@@ -117,32 +131,43 @@ def run(params: Dict):
     # ------------------------------------------------------
     # Load Y data 
     # ------------------------------------------------------
-    y_data = 'new_labels_0_10'
+    #y_data = 'new_labels_0_10'
     # convert to PyTorch data format
-
-    df = pd.read_csv('data/' + y_data + '.csv')
-
+    #df = pd.read_csv('data/' + y_data + '.csv')
 
 
+    # cutoff is 10 per paper -- unhardcode this
+    synergy_bins = [-np.inf, 10, np.inf]
+    synergy_labels = [0, 1]
+    y_data['label'] = pd.cut(np.array(y_data[params['y_col_name']]), bins=synergy_bins, labels=synergy_labels)
+    
+    y_data = y_data.merge(smiles, how='inner', left_on='DrugID_row', right_on='DrugID')
+    y_data = y_data.drop('DrugID', axis=1)
+    y_data = y_data.rename(columns={'smiles': 'drug1'})
+    y_data = y_data.merge(smiles, how='inner', left_on='DrugID_col', right_on='DrugID')
+    y_data = y_data.drop('DrugID', axis=1)
+    y_data = y_data.rename(columns={'smiles': 'drug2'})
+    y_data = y_data.rename(columns={'DepMapID': 'cell'})
 
-
+    small_y_data = y_data[['drug1', 'drug2', 'cell', 'label']]
     # ------------------------------------------------------
     # Construct ML data for every stage (train, val, test)
     # ------------------------------------------------------
 
-    lenth = len(df)
-    pot = int(lenth/5)
-    print('lenth', lenth)
-    print('pot', pot)
+    #lenth = len(df)
+    #pot = int(lenth/5)
+    #print('lenth', lenth)
+    #print('pot', pot)
+    #random_num = random.sample(range(0, lenth), lenth)
+    #i=0
+    #test_num = random_num[pot*i:pot*(i+1)]
+    #train_num = random_num[:pot*i] + random_num[pot*(i+1):]
+    #df_train = df.iloc[train_num]
+    #df_test = df.iloc[test_num]
 
-    random_num = random.sample(range(0, lenth), lenth)
-    #for i in range(1):
-    i=0
-    test_num = random_num[pot*i:pot*(i+1)]
-    train_num = random_num[:pot*i] + random_num[pot*(i+1):]
-
-    df_train = df.iloc[train_num]
-    df_test = df.iloc[test_num]
+    df_train = small_y_data[small_y_data['split'] == 'train']
+    df_val = small_y_data[small_y_data['split'] == 'val']
+    df_test = small_y_data[small_y_data['split'] == 'test']
 
     drug1_train, drug2_train, cell_train, label_train = list(df_train['drug1']), list(df_train['drug2']), list(df_train['cell']), list(df_train['label'])
     drug1_train, drug2_train, cell_train, label_train = np.asarray(drug1_train), np.asarray(drug2_train), np.asarray(cell_train), np.asarray(label_train)
@@ -160,8 +185,18 @@ def run(params: Dict):
 
     print("TEST")
     print('开始创建数据 - Start creating data')
-    drug1_data_test = TestbedDataset(root=params['output_dir'], dataset='drug1_test', xd=drug1_test, xt=cell_test, xt_featrue=cell_features, y=label_test,smile_graph=smile_graph)
-    drug2_data_test = TestbedDataset(root=params['output_dir'], dataset='drug2_test', xd=drug2_test, xt=cell_test, xt_featrue=cell_features, y=label_test,smile_graph=smile_graph)
+    drug1_data_test = TestbedDataset(root=params['output_dir'], dataset='drug1_test', xd=drug1_test, xt=cell_test, xt_featrue=cell_features, y=label_test, smile_graph=smile_graph)
+    drug2_data_test = TestbedDataset(root=params['output_dir'], dataset='drug2_test', xd=drug2_test, xt=cell_test, xt_featrue=cell_features, y=label_test, smile_graph=smile_graph)
+    print('创建数据成功 - Data created successfully')
+
+    drug1_val, drug2_val, cell_val, label_val = list(df_val['drug1']), list(df_val['drug2']), list(df_val['cell']), list(df_val['label'])
+    drug1_val, drug2_val, cell_val, label_val = np.asarray(drug1_val), np.asarray(drug2_val), np.asarray(cell_val), np.asarray(label_val)
+    # make data PyTorch Geometric ready
+
+    print("VAL")
+    print('开始创建数据 - Start creating data')
+    drug1_data_val = TestbedDataset(root=params['output_dir'], dataset='drug1_val', xd=drug1_val, xt=cell_val, xt_featrue=cell_features, y=label_val, smile_graph=smile_graph)
+    drug2_data_val = TestbedDataset(root=params['output_dir'], dataset='drug2_val', xd=drug2_val, xt=cell_val, xt_featrue=cell_features, y=label_val, smile_graph=smile_graph)
     print('创建数据成功 - Data created successfully')
 
     return params["output_dir"]
@@ -169,10 +204,10 @@ def run(params: Dict):
 
 # [Req]
 def main(args):
-    cfg = DRPPreprocessConfig()
+    cfg = SynergyPreprocessConfig()
     params = cfg.initialize_parameters(
         pathToModelDir=filepath,
-        default_config="deepdds_params.txt",
+        default_config="deepdds_params.ini",
         additional_definitions=preprocess_params)
     ml_data_outdir = run(params)
     print("\nFinished data preprocessing.")
