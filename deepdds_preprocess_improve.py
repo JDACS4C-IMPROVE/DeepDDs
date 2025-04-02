3156bf6091b9f5677fd35ee5c6bf0365e4a19545
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-04-02 14:29:16 -0400

    testing bug

--- deepdds_preprocess_improve.py
+++ deepdds_preprocess_improve.py
@@ -107,16 +107,17 @@ def run(params: Dict):
                      benchmark_dir = params['input_dir'], 
                      drug_column_name = params['drug_column_name'])
     
-    #file2 = 'data/independent_set/independent_cell_features_954.csv'
-    #cell_features = []
-    #with open(file2) as csvfile:
-    #    csv_reader = csv.reader(csvfile)  # 使用csv.reader读取csvfile中的文件
-    #    for row in csv_reader:
-    #        cell_features.append(row)
-    #cell_features = np.array(cell_features)
-    #print('cell_features', cell_features)
+    file2 = 'data/independent_set/independent_cell_features_954.csv'
+    cell_features2 = []
+    with open(file2) as csvfile:
+        csv_reader = csv.reader(csvfile)  # 使用csv.reader读取csvfile中的文件
+        for row in csv_reader:
+            cell_features2.append(row)
+    print('first cell_features2', cell_features2)
+    cell_features2 = np.array(cell_features2)
+    print('second cell_features2', cell_features2)
     cell_features = np.array(cell_feature)
-
+    sys.exit()
     #compound_iso_smiles = []
     #df = pd.read_csv('data/smiles.csv')
     drug_feature_cleaned = drug_feature.dropna(subset=[drug_feature.columns[0]])
