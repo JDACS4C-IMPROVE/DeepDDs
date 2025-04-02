fccbf16d557b825d34da7cd31ffd4aaaaa63db37
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-04-02 16:39:22 -0400

    test

--- deepdds_train_improve.py
+++ deepdds_train_improve.py
@@ -138,7 +138,9 @@ def run(params):
     #num_features_xt = drug1_loader_train.x
     print("NCK1", drug1_data_train)
 
-    print("NCK2", drug1_data_train.cell.shape)
+    print("NCK2", drug1_data_train.cell.shape[1])
+    print("NCK3", drug1_data_train.edge_index.shape)
+
     model = modeling().to(device)
     global loss_fn
     loss_fn = nn.CrossEntropyLoss()
