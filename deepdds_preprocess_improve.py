608f151514a99b81b46e8f8a597d89d249b4d5aa
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-04-02 15:20:07 -0400

    bug fix

--- deepdds_preprocess_improve.py
+++ deepdds_preprocess_improve.py
@@ -154,7 +154,7 @@ def run(params: Dict):
     #y_data = y_data.drop('DrugID', axis=1)
     y_data = y_data.rename(columns={'smiles': 'drug2'})
     y_data = y_data.rename(columns={'DepMapID': 'cell'})
-
+    y_data = y_data[y_data[params['cell_column_name']].isin(cell_feature.index.to_list())]
     small_y_data = y_data[['drug1', 'drug2', 'cell', 'label', 'split']]
     # ------------------------------------------------------
     # Construct ML data for every stage (train, val, test)
