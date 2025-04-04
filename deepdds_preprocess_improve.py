a4c04af6e558ae983734d6d38f28789b64bb3f68
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-04-04 14:46:15 -0400

    cleaning up

--- deepdds_preprocess_improve.py
+++ deepdds_preprocess_improve.py
@@ -13,79 +13,28 @@ import improvelib.utils as frm
 # Model-specific imports
 import csv
 from itertools import islice
-import joblib
+#import joblib
 import pandas as pd
 import numpy as np
 #import os
 #import json, pickle
-from collections import OrderedDict
-from rdkit import Chem
-# from rdkit.Chem import MolFromSmiles
-import networkx as nx
+#from collections import OrderedDict
+
 from utils_test import TestbedDataset
 import random
 from random import shuffle
-import torch.utils.data as Data
-import torch
+#import torch.utils.data as Data
+#import torch
 import torch.nn.functional as F
 import torch.nn as nn
-from torch.utils.data import TensorDataset, Dataset
-import torch_geometric.deprecation
-
+#from torch.utils.data import TensorDataset, Dataset
+#import torch_geometric.deprecation
+from utils_preprocess import smile_to_graph
 from model_params_def import preprocess_params
 filepath = Path(__file__).resolve().parent # [Req]
 
 
 
-def get_cell_feature(cellId, cell_features):
-    for row in islice(cell_features, 0, None):
-        if row[0] == cellId:
-            return row[1: ]
-
-def atom_features(atom):
-    return np.array(one_of_k_encoding_unk(atom.GetSymbol(),
-                                          ['C', 'N', 'O', 'S', 'F', 'Si', 'P', 'Cl', 'Br', 'Mg', 'Na', 'Ca', 'Fe', 'As',
-                                           'Al', 'I', 'B', 'V', 'K', 'Tl', 'Yb', 'Sb', 'Sn', 'Ag', 'Pd', 'Co', 'Se',
-                                           'Ti', 'Zn', 'H', 'Li', 'Ge', 'Cu', 'Au', 'Ni', 'Cd', 'In', 'Mn', 'Zr', 'Cr',
-                                           'Pt', 'Hg', 'Pb', 'Unknown']) +
-                    one_of_k_encoding(atom.GetDegree(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) +
-                    one_of_k_encoding_unk(atom.GetTotalNumHs(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) +
-                    one_of_k_encoding_unk(atom.GetImplicitValence(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) +
-                    [atom.GetIsAromatic()])
-
-
-def one_of_k_encoding(x, allowable_set):
-    if x not in allowable_set:
-        raise Exception("input {0} not in allowable set{1}:".format(x, allowable_set))
-    return list(map(lambda s: x == s, allowable_set))
-
-
-def one_of_k_encoding_unk(x, allowable_set):
-    """Maps inputs not in the allowable set to the last element."""
-    if x not in allowable_set:
-        x = allowable_set[-1]
-    return list(map(lambda s: x == s, allowable_set))
-
-
-def smile_to_graph(smile):
-    mol = Chem.MolFromSmiles(smile)
-
-    c_size = mol.GetNumAtoms()
-
-    features = []
-    for atom in mol.GetAtoms():
-        feature = atom_features(atom)
-        features.append(feature / sum(feature))
-
-    edges = []
-    for bond in mol.GetBonds():
-        edges.append([bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()])
-    g = nx.Graph(edges).to_directed()
-    edge_index = []
-    for e1, e2 in g.edges:
-        edge_index.append([e1, e2])
-
-    return c_size, features, edge_index
 
 
 # [Req]
