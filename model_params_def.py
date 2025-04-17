50f68f249b06699a5886b3e801f49a534658eee4
nkoussa <156325369+nkoussa@users.noreply.github.com>
2025-04-17 15:58:52 -0400

    changed cutoff

--- model_params_def.py
+++ model_params_def.py
@@ -2,9 +2,9 @@ from improvelib.utils import str2bool
 
 preprocess_params = [
     {"name": "cutoff",
-     "type": int,
-     "default": 10,
-     "help": "Cutoff for binarization. Default is 10 as per paper.",
+     "type": float,
+     "default": 9.24,
+     "help": "Cutoff for binarization. 10 was used in the paper, 9.24 is used for updated DrugComb v1.5 to allow for the same number of positive samples. ",
     },
 ]
 
